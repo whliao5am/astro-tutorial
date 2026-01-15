76e841a4b092fa54270a5ed1d696a721e4d4a8dd
whliao5am <wh-liao@outlook.com>
2026-01-15 12:56:47 +0800

    Add GitHub Pages deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://whliao5am.github.io",
+  base: "/astro-tutorial",
   integrations: [preact()]
 });
